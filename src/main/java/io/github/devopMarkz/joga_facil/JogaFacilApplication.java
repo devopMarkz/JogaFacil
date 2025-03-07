47fd59fe143c2e0206d0d79ce5116bf35e95a22a
devopMarkz <marcosdev2002@gmail.com>
2025-03-07 11:30:41 -0300

    First commit

---
+++ src/main/java/io/github/devopMarkz/joga_facil/JogaFacilApplication.java
@@ -0,0 +1,13 @@
+package io.github.devopMarkz.joga_facil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogaFacilApplication.class, args);
+	}
+
+}
