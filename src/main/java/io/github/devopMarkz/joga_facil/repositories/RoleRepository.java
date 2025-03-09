c4b4e3aed829f93b1f98957b5f76a0a1c4518daf
devopMarkz <marcosdev2002@gmail.com>
2025-03-09 01:47:31 -0300

    Implementando busca de Role por Authority

--- src/main/java/io/github/devopMarkz/joga_facil/repositories/RoleRepository.java
+++ src/main/java/io/github/devopMarkz/joga_facil/repositories/RoleRepository.java
@@ -1,7 +1,13 @@
 package io.github.devopMarkz.joga_facil.repositories;
 
 import io.github.devopMarkz.joga_facil.model.Role;
+import io.github.devopMarkz.joga_facil.model.enums.RoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByAuthority(RoleEnum authority);
+
 }
