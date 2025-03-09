d75d16dee06f33f63e1e83a57efb4ff16ddc3beb
devopMarkz <marcosdev2002@gmail.com>
2025-03-08 21:56:59 -0300

    Criando Repository para Role

---
+++ src/main/java/io/github/devopMarkz/joga_facil/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package io.github.devopMarkz.joga_facil.repositories;
+
+import io.github.devopMarkz.joga_facil.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
