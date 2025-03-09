fd7b8dd9cc6fe14f7c9e6ddc91c2b5ebd5676874
devopMarkz <marcosdev2002@gmail.com>
2025-03-08 23:37:21 -0300

    Implementando UserDetailsService

--- src/main/java/io/github/devopMarkz/joga_facil/services/UserDetailsServiceImpl.java
+++ src/main/java/io/github/devopMarkz/joga_facil/services/UserDetailsServiceImpl.java
@@ -1,4 +1,24 @@
 package io.github.devopMarkz.joga_facil.services;
 
-public class UserDetailsServiceImpl {
+import io.github.devopMarkz.joga_facil.repositories.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UserDetailsServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.searchByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário inexistente."));
+    }
+
 }
