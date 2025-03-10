81085d79de79b9799a2a8956864c26ad9da3f3b0
devopMarkz <marcosdev2002@gmail.com>
2025-03-10 10:52:10 -0300

    Criando servico de Token

--- src/main/java/io/github/devopMarkz/joga_facil/controllers/AuthenticationController.java
+++ src/main/java/io/github/devopMarkz/joga_facil/controllers/AuthenticationController.java
@@ -1,6 +1,7 @@
 package io.github.devopMarkz.joga_facil.controllers;
 
 import io.github.devopMarkz.joga_facil.dtos.auth.AuthDTO;
+import io.github.devopMarkz.joga_facil.services.impl.TokenServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,14 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthenticationController {
 
-    @Autowired
     private AuthenticationManager authenticationManager;
+    private TokenServiceImpl tokenService;
+
+    public AuthenticationController(AuthenticationManager authenticationManager, TokenServiceImpl tokenService) {
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
+    }
 
     @PostMapping("/login")
     public String login(@RequestBody AuthDTO authDTO){
         var auth = new UsernamePasswordAuthenticationToken(authDTO.login(), authDTO.senha());
         authenticationManager.authenticate(auth);
-        return "Ok";
+        return tokenService.obterToken(authDTO);
     }
 
 }
---
+++ src/main/java/io/github/devopMarkz/joga_facil/services/impl/TokenServiceImpl.java
@@ -0,0 +1,59 @@
+package io.github.devopMarkz.joga_facil.services.impl;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import io.github.devopMarkz.joga_facil.dtos.auth.AuthDTO;
+import io.github.devopMarkz.joga_facil.dtos.role.RoleDTO;
+import io.github.devopMarkz.joga_facil.exceptions.ResourceNotFoundException;
+import io.github.devopMarkz.joga_facil.model.Role;
+import io.github.devopMarkz.joga_facil.model.Usuario;
+import io.github.devopMarkz.joga_facil.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Service
+public class TokenServiceImpl {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public String obterToken(AuthDTO authDTO){
+        var usuario = usuarioRepository.searchByEmail(authDTO.login())
+                .orElseThrow(() -> new ResourceNotFoundException("Usuário inexistente."));
+        return gerarToken(usuario);
+    }
+
+    public String obterLogin(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+            return JWT.require(algorithm)
+                    .withIssuer("joga-facil-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTCreationException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String gerarToken(Usuario usuario){
+        List<String> roles = usuario.getRoles().stream().map(Role::getAuthority).toList();
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+            return JWT.create()
+                    .withIssuer("joga-facil-api")
+                    .withSubject(usuario.getEmail())
+                    .withClaim("roles", roles)
+                    .withExpiresAt(Instant.now().plus(2L, ChronoUnit.HOURS))
+                    .sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
