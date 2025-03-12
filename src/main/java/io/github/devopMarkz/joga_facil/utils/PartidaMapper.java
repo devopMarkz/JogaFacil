2f75aa1c9a9b7a8a0c916e45dc2fa2f3ed6f8655
devopMarkz <marcosdev2002@gmail.com>
2025-03-12 19:00:24 -0300

    Criando Mapper para Partida

---
+++ src/main/java/io/github/devopMarkz/joga_facil/utils/PartidaMapper.java
@@ -0,0 +1,52 @@
+package io.github.devopMarkz.joga_facil.utils;
+
+import io.github.devopMarkz.joga_facil.dtos.partida.PartidaRequestDTO;
+import io.github.devopMarkz.joga_facil.dtos.partida.PartidaResponseDTO;
+import io.github.devopMarkz.joga_facil.dtos.participantepartida.ParticipantePartidaDTO;
+import io.github.devopMarkz.joga_facil.dtos.usuario.UsuarioOrganizadorDTO;
+import io.github.devopMarkz.joga_facil.model.Partida;
+import io.github.devopMarkz.joga_facil.model.Usuario;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PartidaMapper {
+
+    public Partida toEntity(PartidaRequestDTO dto, Usuario organizador) {
+        return new Partida(
+                dto.dataHora(),
+                dto.local(),
+                dto.custoTotal(),
+                dto.vagasDisponiveis(),
+                organizador
+        );
+    }
+
+    public PartidaResponseDTO toDTO(Partida partida) {
+        UsuarioOrganizadorDTO organizadorDTO = new UsuarioOrganizadorDTO(
+                partida.getOrganizador().getNome(),
+                partida.getOrganizador().getEmail(),
+                partida.getOrganizador().getTelefone()
+        );
+
+        List<ParticipantePartidaDTO> participantesDTO = partida.getParticipantes().stream()
+                .map(participante -> new ParticipantePartidaDTO(
+                        participante.getUsuario().getNome(),
+                        participante.getUsuario().getEmail(),
+                        participante.getUsuario().getTelefone(),
+                        participante.getStatusPagamento(),
+                        participante.getConfirmacaoPresenca()
+                )).toList();
+
+        return new PartidaResponseDTO(
+                partida.getId(),
+                partida.getDataHora(),
+                partida.getLocal(),
+                partida.getCustoTotal(),
+                partida.getVagasDisponiveis(),
+                organizadorDTO,
+                participantesDTO
+        );
+    }
+}
