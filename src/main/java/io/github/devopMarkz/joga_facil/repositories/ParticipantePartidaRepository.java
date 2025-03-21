d6dfeff0b8031c0ca9e194616dbbd0f85df381a3
devopMarkz <marcosdev2002@gmail.com>
2025-03-20 21:31:34 -0300

    Criacao de metodo para busca de partida por id de usuario

--- src/main/java/io/github/devopMarkz/joga_facil/repositories/ParticipantePartidaRepository.java
+++ src/main/java/io/github/devopMarkz/joga_facil/repositories/ParticipantePartidaRepository.java
@@ -2,6 +2,9 @@ package io.github.devopMarkz.joga_facil.repositories;
 
 import io.github.devopMarkz.joga_facil.model.ParticipantePartida;
 import io.github.devopMarkz.joga_facil.model.ParticipantePartidaId;
+import io.github.devopMarkz.joga_facil.model.Partida;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,5 +17,12 @@ public interface ParticipantePartidaRepository extends JpaRepository<Participant
             "FROM tb_participante_partida " +
             "WHERE tb_participante_partida.partida_id = :partidaId", nativeQuery = true)
     long countParticipantesByPartidaId(@Param("partidaId") Long partidaId);
+
+    @Query(value = "SELECT tb_partida.* FROM tb_participante_partida " +
+            "INNER JOIN tb_partida " +
+            "ON tb_participante_partida.partida_id = tb_partida.id " +
+            "WHERE tb_participante_partida.usuario_id = :usuarioId", nativeQuery = true)
+    Page<Partida> searchPartidasByParticipante(@Param("usuarioId") Long usuarioId,
+                                               Pageable pageable);
 
 }
